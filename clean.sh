b1c88f70d72946d55d718510bf46746324e64ddc
Tickloop <pulkitarya2000@gmail.com>
2022-01-30 13:41:30 -0500

    Script to clean the pipeline: Use with caution

---
+++ clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# remove all figures
+rm -rf figures/celeb64/* 
+rm -rf figures/simpsons_128/*
+rm -rf figures/simpsons_simplified_64/*
+
+# remove all gifs
+rm -rf gifs/celeb64/* 
+rm -rf gifs/simpsons_128/*
+rm -rf gifs/simpsons_simplified_64/*
+
+# remove all models
+rm -rf models/celeb64/* 
+rm -rf models/simpsons_128/*
+rm -rf models/simpsons_simplified_64/*
+
+# remove all plots
+rm -rf plots/celeb64/* 
+rm -rf plots/simpsons_128/*
+rm -rf plots/simpsons_simplified_64/*
+
+# remove all samples
+rm -rf samples/celeb64/* 
+rm -rf samples/simpsons_128/*
+rm -rf samples/simpsons_simplified_64/*
